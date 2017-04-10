6d8aab705aa6a41d9aec48642e260c7258cf6cfd
Yogesh R Nitnaware <yr-nitnaware@wiu.edu>
2017-04-10 14:13:17 -0500

    initial commit

---
+++ routes/api/users.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var router = require('express').Router();
+router.use('/', require('./users'));
+
+var passport = require('passport');
+var User = mongoose.model('User');
+var auth = require('../auth');
+
+router.post('/users', function(req, res, next){
+  var user = new User();
+
+  user.username = req.body.user.username;
+  user.email = req.body.user.email;
+  user.setPassword(req.body.user.password);
+
+  user.save().then(function(){
+    return res.json({user: user.toAuthJSON()});
+  }).catch(next);
+});
+
+
+router.post('/users/login', function(req, res, next){
+  if(!req.body.user.email){
+    return res.status(422).json({errors: {email: "can't be blank"}});
+  }
+
+  if(!req.body.user.password){
+    return res.status(422).json({errors: {password: "can't be blank"}});
+  }
+
+  passport.authenticate('local', {session: false}, function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      user.token = user.generateJWT();
+      return res.json({user: user.toAuthJSON()});
+    } else {
+      return res.status(422).json(info);
+    }
+  })(req, res, next);
+});
+
+module.exports = router;
