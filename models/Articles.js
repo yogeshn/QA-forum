f4230779c8eac7f9610fdc67170327862f372b38
Yogesh R Nitnaware <yr-nitnaware@wiu.edu>
2017-04-22 14:51:16 -0500

    articles created

---
+++ models/Articles.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var slug = require('slug'); // package we'll use to auto create URL slugs
+
+var ArticleSchema = new mongoose.Schema({
+  slug: {type: String, lowercase: true, unique: true},
+  title: String,
+  description: String,
+  body: String,
+  favoritesCount: {type: Number, default: 0},
+  tagList: [{ type: String }],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, {timestamps: true});
+
+ArticleSchema.plugin(uniqueValidator, {message: 'is already taken'});
+
+
+mongoose.model('Article', ArticleSchema);
