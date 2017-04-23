f77ddb1705776371fd13fafec53888bbbcf4b67c
Yogesh R Nitnaware <yr-nitnaware@wiu.edu>
2017-04-23 16:18:33 -0500

    favorite schema

--- models/User.js
+++ models/User.js
@@ -62,4 +62,13 @@ UserSchema.methods.toProfileJSONFor = function(user){
   };
 };
 
+
+UserSchema.methods.favorite = function(id){
+  if(this.favorites.indexOf(id) === -1){
+    this.favorites.push(id);
+  }
+
+  return this.save();
+};
+
 mongoose.model('User', UserSchema);
