f2eb98a464c876ece818e2ed6d76e5264237cc39
Yogesh R Nitnaware <yr-nitnaware@wiu.edu>
2017-05-04 17:12:20 -0500

    comments and tags added

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 var passport = require('passport');
 var mongoose = require('mongoose');
 var Article = mongoose.model('Article');
+var Comment = mongoose.model('Comment');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
